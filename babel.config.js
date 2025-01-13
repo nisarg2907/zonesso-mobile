749dde107a00539388d453c653a308e92a88280e
Nisarg <nisargshah.ict19@gmail.com>
2025-01-13 13:46:48 +0530

    zustand state management setup for the app

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,5 @@
+const plugin = require("tailwindcss");
+
 module.exports = function (api) {
     api.cache(true);
     return {
@@ -5,5 +7,17 @@ module.exports = function (api) {
         ["babel-preset-expo", { jsxImportSource: "nativewind" }],
         "nativewind/babel",
       ],
+      // plugin:[  [
+      //   'module:react-native-dotenv',
+      //   {
+      //     moduleName: '@env',
+      //     path: '.env',
+      //     blocklist: null,
+      //     allowlist: null,
+      //     safe: false,
+      //     allowUndefined: true,
+      //     verbose: false,
+      //   },
+      // ],],
     };
   };
